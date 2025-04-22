8e1bc74a460aba76358b52eec1da7289d10da881
Callum Flack <callum@callumflack.design>
2025-04-23 09:00:13 +1000

    use ZoomCarousel on home page

--- src/app/(home)/home-page.tsx
+++ src/app/(home)/home-page.tsx
@@ -2,14 +2,13 @@
 
 import { Link, Text } from "@/components/atoms";
 import { TitleHeader } from "@/components/elements";
-import { EnhancedZoomable } from "@/components/media/zoomable-enhanced";
 import { Intro, Outro, PageInner, PageWrapper } from "@/components/page";
 import { PostBlock, PostLine } from "@/components/post";
-import { Slider } from "@/components/slider";
 import { BodyClassProvider } from "@/components/utils/body-class-provider";
 import { Post } from "content-collections";
 import { cx } from "cva";
 import { useState } from "react";
+import { ZoomCarousel } from "./zoom-carousel";
 
 export const HomePage = ({
   latestPost,
@@ -110,22 +109,9 @@ export const HomePage = ({
                 className="w-full h-[400px] bg-background-hover rounded-md"
                 style={{ aspectRatio: "16/9" }} // Match slider aspect ratio
               ></div> */}
-            <EnhancedZoomable
-              maxWidth="full"
-              scaleAmount={2}
-              isControlled={true}
-              isZoomed={isZoomed}
-              fullWidthWhenZoomed={true}
-              onZoomChange={setIsZoomed}
-              // min-h avoids hydration jank
-              className="z-2 min-h-[276px] sm:min-h-[331px]"
-            >
-              <Slider
-                projects={projects}
-                showInFull={false}
-                isZoomed={isZoomed}
-              />
-            </EnhancedZoomable>
+            {/* // min-h avoids hydration jank
+              className="z-2 min-h-[276px] sm:min-h-[331px]"? */}
+            <ZoomCarousel projects={projects} />
           </div>
           <RuleWithinInner />
         </PageInner>
